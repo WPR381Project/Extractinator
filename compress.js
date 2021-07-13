42ac4fcf29d9f094056e3eaf96d0f09580f6f678
Tian Bornman <tianbornman.tb@gmail.com>
2021-07-13 20:06:47 +0200

    compressing subfolders aswell

--- compress.js
+++ compress.js
@@ -1,24 +1,65 @@
 var file_system = require('fs');
 var archiver = require('archiver');
+const path = require('path');
 
-var output = file_system.createWriteStream('tozip.zip');
 var archive = archiver('zip');
-
-output.on('close', function () {
-    console.log(archive.pointer() + ' total bytes');
-    console.log('archiver has been finalized and the output file descriptor has closed.');
-});
 
 archive.on('error', function(err){
     throw err;
 });
 
-archive.pipe(output);
+const SubFolders = () => {
+    return new Promise((resolve, reject) => {
+        file_system.readdir('tozip', (err, files) => {
+  
+            for (let i = 0; i < files.length; i++) {
+                if (path.extname(files[i]) != '') {
+                
+                }
+                else 
+                {
+                    var currentArchive = archiver('zip');
+                
+                    var currentFile = file_system.createWriteStream('tozip/' + files[i] + '.zip');
+                
+                    currentArchive.pipe(currentFile);
+                
+                    currentArchive.directory('tozip/' + files[i], false);
+                
+                    currentArchive.finalize();
+                
+                    currentFile.close();
+                }
+            } 
+        });
+        resolve();
+    })
+}
 
-// append files from a sub-directory, putting its contents at the root of archive
-archive.directory('tozip', false);
+const MainFolder = () => {
+    return new Promise((resolve, reject) => {
+        var output = file_system.createWriteStream('tozip.zip');
 
-// append files from a sub-directory and naming it `new-subdir` within the archive
-archive.directory('subdir/', 'new-subdir');
+        output.on('close', function () {
+            console.log(archive.pointer() + ' total bytes');
+            console.log('archiver has been finalized and the output file descriptor has closed.');
+        });
 
-archive.finalize();
+        archive.pipe(output);
+
+        // append files from a sub-directory, putting its contents at the root of archive
+        archive.directory('tozip', false);
+                
+        // append files from a sub-directory and naming it `new-subdir` within the archive
+        archive.directory('subdir/', 'new-subdir');
+                
+        archive.finalize();
+        resolve();
+    })
+}
+
+
+(async () => {
+    await SubFolders();
+    await MainFolder();
+})()
