3827dea426a2ad4a0184e56843fab5fb99ff98d0
Mesabe <bbgunbrits@gmail.com>
2021-07-13 18:17:32 +0200

    1st

---
+++ extraction.js
@@ -0,0 +1,36 @@
+async function extractZip(source, target) 
+{
+    try 
+    {
+      await extract(source, { dir: target });
+      console.log("Extraction complete");
+    } catch (err) 
+    {
+      console.log("Oops: extractZip failed", err);
+    }
+  }
+
+  const unzipFiles = async function (dirPath) 
+  {
+    const files = fs.readdirSync(dirPath);
+  
+    await Promise.all(
+      files.map(async (file) => 
+      {
+        if (fs.statSync(dirPath + "/" + file).isDirectory()) 
+        {
+          await unzipFiles(dirPath + "/" + file);
+        } else 
+        {
+          const fullFilePath = path.join(dirPath, "/", file);
+          const folderName = file.replace(".zip", "");
+          if (file.endsWith(".zip")) 
+          {
+            zippedFiles.push(folderName);
+            await extractZip(fullFilePath, path.join(dirPath, "/", folderName));
+            await unzipFiles(path.join(dirPath, "/", folderName));
+          }
+        }
+      })
+    );
+  };
