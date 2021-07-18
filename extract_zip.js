d6bacf244a66816e4e65cd2c7845670341647a0b
Kyle-van-Niekerk-578224 <79686783+Kyle-van-Niekerk-578224@users.noreply.github.com>
2021-07-18 12:08:31 +0200

    Update extract_zip.js

--- extract_zip.js
+++ extract_zip.js
@@ -1,4 +1,4 @@
-const decompress = require("decompress");
+/*const decompress = require("decompress");
 
 //Simple version:
  /*
@@ -8,7 +8,7 @@ const decompress = require("decompress");
  */
 
  //More complex, error handling + filter to exclude file types:
- const path = require("path");
+ /*const path = require("path");
 
 (async () => {
 
@@ -21,4 +21,66 @@ const decompress = require("decompress");
         console.log(error);
     }
 
-})();
+})();*/
+
+const { SSL_OP_EPHEMERAL_RSA } = require('constants');
+const decompress = require('decompress');
+const fs = require('fs');
+const path = require('path');
+const { extname, basename, dirname } = require('path/posix');
+const dirpath = "./project_folders";
+
+
+function extractZip(source, target)
+{
+  (async () => {
+    //var sourceParameter = path.format(source);
+      try {
+          const files = await decompress(source,target);
+              //filter: file => path.extname(file.path) == ".zip";
+          console.log(files);
+      } catch (error) {
+          console.log(error);
+      }
+  
+  })();
+}
+
+  const unzipFiles = async function (dirPath) 
+  {
+    const files = fs.readdirSync(dirPath);
+  
+    await Promise.all(
+      files.map(async (file) => 
+      {
+        setTimeout(() => {
+          files.forEach(file => {
+            console.log(file);       
+          });
+        }, 1000);
+        
+        if (fs.statSync(dirPath + "/" + file).isDirectory())
+        {
+          
+          await unzipFiles(dirPath + "/" + file);
+
+        } else 
+        {
+          const target = dirPath;
+          const folderName = basename(file);
+          const source = (dirPath+"/"+folderName);
+          console.log("fullFilePath : ("+target+")   \nfoldername : ("+folderName+")   \nsource :"+source);
+          if (source.endsWith(".zip")) 
+          {
+            extractZip(source, target);
+            setTimeout(() => {
+              unzipFiles(target+"/"+basename(file));
+            }, 1000);
+            
+          }
+        }
+      })
+    );
+  };
+//extractZip("./project_folders/Num5.zip", "./project_folders")
+unzipFiles(dirpath);
